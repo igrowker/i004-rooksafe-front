3e801c29dd7713375ed6399830b1e61aa2e48623
Ivan <ivanlopezokk@gmail.com>
2024-11-15 18:05:41 -0300

    RegisterComponent agregado

---
+++ src/app/components/register-component/register-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-component.component.html',
+  styleUrl: './register-component.component.css'
+})
+export class RegisterComponentComponent {
+
+}
