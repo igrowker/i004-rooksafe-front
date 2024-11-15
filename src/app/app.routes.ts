ee252f058147e19d254fd53fd95d3ffdc12346d8
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2024-11-15 20:26:31 -0300

    Config Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '@features/home/home.component';
+import { ErrorComponent } from './features/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'error', component: ErrorComponent },
+    { path: '**', redirectTo: 'error' }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { materialProviders } from './app/shared/material/material.module';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideAnimations(),
-    materialProviders 
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
