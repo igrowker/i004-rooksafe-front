89d10bd6224a4182bd77c0353ffa2d41c52c9fc3
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2024-11-19 20:17:05 -0300

    fix bugs in home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 import { MaterialModule } from '@shared/material/material.module';
 import { CardComponent } from 'src/app/components/card/card.component';
 import { AuthModalComponent } from 'src/app/components/auth-modal/auth-modal.component';
+import { MatDialog } from '@angular/material/dialog';
 
 
-@Component(
+@Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, MaterialModule, CardComponent,AuthModalComponent],
@@ -28,10 +29,6 @@ export class HomeComponent {
       image: 'assets/images/recomendationCard-3.png',
     },
   ];
- openLoginModal() {
-    this.dialog.open(AuthModalComponent),{
-      panelClass: 'custom-dialog-container',
-    };
-  }
+
 
 }
