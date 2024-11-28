dd54c53a7f89d071e3ba26651d4eb7a291e9c7c5
fsalasevicius <flaviosalasevicius@hotmail.com>
2024-11-28 11:59:37 -0300

    agregando perfil de usuario en toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '@shared/material/material.module';
-
+import { AuthService } from '../../services/auth-service';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
@@ -8,4 +8,25 @@ import { MaterialModule } from '@shared/material/material.module';
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css',
 })
-export class ToolbarComponent {}
+
+export class ToolbarComponent {
+  userProfile: any = null; 
+  error: string | null = null;
+
+  constructor(private _userProfileService: AuthService) {}
+
+  ngOnInit(): void {
+    this.loadUserProfile();
+  }
+
+  loadUserProfile(): void {
+    this._userProfileService.get_user().subscribe(
+      (response) => {
+        this.userProfile = response;
+      },
+      (error) => {
+        console.error("Error al obtener el perfil del usuario:", error);
+      }
+    );
+  }
+}
