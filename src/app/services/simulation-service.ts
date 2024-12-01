9afad5d1e2c40261f17dc5d75ee2873fd60e171b
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2024-12-01 11:58:33 -0300

    eliminando archivo global de services

--- src/app/services/simulation-service.ts
+++ src/app/services/simulation-service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
-import { GLOBAL } from "./GLOBAL";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
